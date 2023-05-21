3190681e42b848827f72a8af8da96fc6762af740
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-21 14:25:37 +0200

    added fail batchmint tests

--- test/ERC1155Yul.t.sol
+++ test/ERC1155Yul.t.sol
@@ -54,59 +54,59 @@ contract ERC1155YulTest is Test {
         assertEq(balance, 840);
     }
 
-    // function testBatchMintToEOA() public {
-    //     uint256[] memory ids = new uint256[](5);
-    //     ids[0] = 1337;
-    //     ids[1] = 1338;
-    //     ids[2] = 1339;
-    //     ids[3] = 1340;
-    //     ids[4] = 1341;
+    function testBatchMintToEOA() public {
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
 
-    //     uint256[] memory amounts = new uint256[](5);
-    //     amounts[0] = 100;
-    //     amounts[1] = 200;
-    //     amounts[2] = 300;
-    //     amounts[3] = 400;
-    //     amounts[4] = 500;
+        uint256[] memory amounts = new uint256[](5);
+        amounts[0] = 100;
+        amounts[1] = 200;
+        amounts[2] = 300;
+        amounts[3] = 400;
+        amounts[4] = 500;
 
-    //     erc1155helper.batchMint(address(0xBEEF), ids, amounts, "");
+        erc1155helper.batchMint(address(0xBEEF), ids, amounts, "");
 
-    //     assertEq(erc1155helper.balanceOf(address(0xBEEF), 1337), 100);
-    //     assertEq(erc1155helper.balanceOf(address(0xBEEF), 1338), 200);
-    //     assertEq(erc1155helper.balanceOf(address(0xBEEF), 1339), 300);
-    //     assertEq(erc1155helper.balanceOf(address(0xBEEF), 1340), 400);
-    //     assertEq(erc1155helper.balanceOf(address(0xBEEF), 1341), 500);
-    // }
+        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1337), 100);
+        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1338), 200);
+        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1339), 300);
+        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1340), 400);
+        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1341), 500);
+    }
 
-    // function testBatchBalanceOf() public {
-    //     address[] memory tos = new address[](5);
-    //     tos[0] = address(0xBEEF);
-    //     tos[1] = address(0xCAFE);
-    //     tos[2] = address(0xFACE);
-    //     tos[3] = address(0xDEAD);
-    //     tos[4] = address(0xFEED);
+    function testBatchBalanceOf() public {
+        address[] memory tos = new address[](5);
+        tos[0] = address(0xBEEF);
+        tos[1] = address(0xCAFE);
+        tos[2] = address(0xFACE);
+        tos[3] = address(0xDEAD);
+        tos[4] = address(0xFEED);
 
-    //     uint256[] memory ids = new uint256[](5);
-    //     ids[0] = 1337;
-    //     ids[1] = 1338;
-    //     ids[2] = 1339;
-    //     ids[3] = 1340;
-    //     ids[4] = 1341;
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
 
-    //     erc1155helper.mint(address(0xBEEF), 1337, 100, "");
-    //     erc1155helper.mint(address(0xCAFE), 1338, 200, "");
-    //     erc1155helper.mint(address(0xFACE), 1339, 300, "");
-    //     erc1155helper.mint(address(0xDEAD), 1340, 400, "");
-    //     erc1155helper.mint(address(0xFEED), 1341, 500, "");
+        erc1155helper.mint(address(0xBEEF), 1337, 100, "");
+        erc1155helper.mint(address(0xCAFE), 1338, 200, "");
+        erc1155helper.mint(address(0xFACE), 1339, 300, "");
+        erc1155helper.mint(address(0xDEAD), 1340, 400, "");
+        erc1155helper.mint(address(0xFEED), 1341, 500, "");
 
-    //     uint256[] memory balances = erc1155helper.balanceOfBatch(tos, ids);
+        uint256[] memory balances = erc1155helper.balanceOfBatch(tos, ids);
 
-    //     assertEq(balances[0], 100);
-    //     assertEq(balances[1], 200);
-    //     assertEq(balances[2], 300);
-    //     assertEq(balances[3], 400);
-    //     assertEq(balances[4], 500);
-    // }
+        assertEq(balances[0], 100);
+        assertEq(balances[1], 200);
+        assertEq(balances[2], 300);
+        assertEq(balances[3], 400);
+        assertEq(balances[4], 500);
+    }
 
     function testSafeTransferFromSelf() public {
         erc1155helper.mint(address(this), 1337, 100, "");
@@ -233,10 +233,10 @@ contract ERC1155YulTest is Test {
     }
 
     // ------------------------------------------------- //
-    // ----------- EXPECTED REVERT TESTING ------------- //
+    // -------------- FAIL FUZZ TESTING ---------------- //
     // ------------------------------------------------- //
 
-    function testFailMintToZero(
+    function testFail_Fuzz_MintToZero(
         uint256 id,
         uint256 amount,
         bytes memory data
@@ -244,7 +244,42 @@ contract ERC1155YulTest is Test {
         erc1155helper.mint(address(0), id, amount, data);
     }
 
-    function testFailSafeTransferFromInsufficientBalance(
+    function testFail_Fuzz_BatchMintToZero() public {
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
+
+        uint256[] memory mintAmounts = new uint256[](5);
+        mintAmounts[0] = 100;
+        mintAmounts[1] = 200;
+        mintAmounts[2] = 300;
+        mintAmounts[3] = 400;
+        mintAmounts[4] = 500;
+
+        erc1155helper.batchMint(address(0), ids, mintAmounts, "");
+    }
+
+    function testFail_Fuzz_BatchMintWithArrayMismatch() public {
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
+
+        uint256[] memory amounts = new uint256[](4);
+        amounts[0] = 100;
+        amounts[1] = 200;
+        amounts[2] = 300;
+        amounts[3] = 400;
+
+        erc1155helper.batchMint(address(0xBEEF), ids, amounts, "");
+    }
+
+    function testFail_Fuzz_SafeTransferFromInsufficientBalance(
         address to,
         uint256 id,
         uint256 mintAmount,
@@ -274,7 +309,7 @@ contract ERC1155YulTest is Test {
         );
     }
 
-    function testFailSafeTransferFromSelfInsufficientBalance(
+    function testFail_Fuzz_SafeTransferFromSelfInsufficientBalance(
         address to,
         uint256 id,
         uint256 mintAmount,
@@ -298,7 +333,7 @@ contract ERC1155YulTest is Test {
         );
     }
 
-    function testFailSafeTransferFromToZero(
+    function testFail_Fuzz_SafeTransferFromToZero(
         uint256 id,
         uint256 mintAmount,
         uint256 transferAmount,
