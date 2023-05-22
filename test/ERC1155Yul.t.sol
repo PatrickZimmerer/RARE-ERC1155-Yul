21bb0fb8c6811058547f0400fe50d8a5cb08af4e
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-05-22 22:39:25 +0200

    removed helper to avoid calling multiple contracts

--- test/ERC1155Yul.t.sol
+++ test/ERC1155Yul.t.sol
@@ -5,14 +5,91 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "forge-std/Vm.sol";
 import "./lib/YulDeployer.sol";
-import "./lib/ERC1155Helper.sol";
 
-interface ERC1155 {}
+interface IERC1155 {
+    // ERC-1155 standard functions
+    event TransferSingle(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 id,
+        uint256 value
+    );
+    event ApprovalForAll(
+        address indexed account,
+        address indexed operator,
+        bool approved
+    );
+    event URI(string value, uint256 indexed id);
+    event TransferBatch(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256[] ids,
+        uint256[] values
+    );
+
+    function balanceOf(
+        address account,
+        uint256 id
+    ) external view returns (uint256);
+
+    // must be marked back to view function
+    function balanceOfBatch(
+        address[] calldata accounts,
+        uint256[] calldata ids
+    ) external view returns (uint256[] memory);
+
+    function setApprovalForAll(address operator, bool approved) external;
+
+    function isApprovedForAll(
+        address account,
+        address operator
+    ) external view returns (bool);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external;
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external;
+
+    // custom functions
+    function mint(address to, uint256 id, uint256 amount) external;
+
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata
+    ) external;
+
+    function batchMint(
+        address to,
+        uint256[] calldata id,
+        uint256[] calldata amounts
+    ) external;
+
+    function batchMint(
+        address to,
+        uint256[] calldata id,
+        uint256[] calldata amounts,
+        bytes calldata
+    ) external;
+}
 
 contract ERC1155YulTest is Test {
     YulDeployer yulDeployer = new YulDeployer();
-    ERC1155 erc1155;
-    ERC1155Helper erc1155helper;
+    IERC1155 token;
 
     address alice = address(0x0187);
     address bob = address(0x42069);
@@ -39,8 +116,8 @@ contract ERC1155YulTest is Test {
     event URI(string _value, uint256 indexed _id);
 
     function setUp() public {
-        erc1155 = ERC1155(yulDeployer.deployContract("ERC1155Yul"));
-        erc1155helper = new ERC1155Helper(IERC1155(address(erc1155)));
+        token = IERC1155(yulDeployer.deployContract("ERC1155Yul"));
+
         vm.label(alice, "Alice");
         vm.label(bob, "Bob");
         vm.label(address(this), "TestContract");
@@ -52,31 +129,31 @@ contract ERC1155YulTest is Test {
     function test_Mint() public {
         vm.expectEmit(false, true, true, true);
         emit TransferSingle(address(this), address(0), alice, 1337, 420);
-        erc1155helper.mint(alice, 1337, 420, "");
-        uint256 balance = erc1155helper.balanceOf(alice, 1337);
+        token.mint(alice, 1337, 420, "");
+        uint256 balance = token.balanceOf(alice, 1337);
         assertEq(balance, 420);
-        erc1155helper.mint(alice, 1337, 420, "");
-        balance = erc1155helper.balanceOf(alice, 1337);
+        token.mint(alice, 1337, 420, "");
+        balance = token.balanceOf(alice, 1337);
         assertEq(balance, 840);
     }
 
     // TODO ASK QUESTIONS WHY OPERATOR IS NOT EMITTED CORRECTLY EVEN THOUGH USED AS ABOVE
-    // function testBatchMintToEOAAndEmit() public {
-    //     uint256[] memory ids = new uint256[](2);
-    //     ids[0] = 1337;
-    //     ids[1] = 1338;
+    function testBatchMintToEOAAndEmit() public {
+        uint256[] memory ids = new uint256[](2);
+        ids[0] = 1337;
+        ids[1] = 1338;
 
-    //     uint256[] memory amounts = new uint256[](2);
-    //     amounts[0] = 100;
-    //     amounts[1] = 200;
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 100;
+        amounts[1] = 200;
 
-    //     vm.expectEmit(false, true, true, true);
-    //     emit TransferBatch(address(this), address(0), alice, ids, amounts);
-    //     erc1155helper.batchMint(alice, ids, amounts, "");
+        vm.expectEmit(false, true, true, true);
+        emit TransferBatch(address(this), address(0), alice, ids, amounts);
+        token.batchMint(alice, ids, amounts, "");
 
-    //     assertEq(erc1155helper.balanceOf(alice, 1337), 100);
-    //     assertEq(erc1155helper.balanceOf(alice, 1338), 200);
-    // }
+        assertEq(token.balanceOf(alice, 1337), 100);
+        assertEq(token.balanceOf(alice, 1338), 200);
+    }
 
     function testBatchMintToEOA() public {
         uint256[] memory ids = new uint256[](5);
@@ -93,13 +170,13 @@ contract ERC1155YulTest is Test {
         amounts[3] = 400;
         amounts[4] = 500;
 
-        erc1155helper.batchMint(alice, ids, amounts, "");
+        token.batchMint(alice, ids, amounts, "");
 
-        assertEq(erc1155helper.balanceOf(alice, 1337), 100);
-        assertEq(erc1155helper.balanceOf(alice, 1338), 200);
-        assertEq(erc1155helper.balanceOf(alice, 1339), 300);
-        assertEq(erc1155helper.balanceOf(alice, 1340), 400);
-        assertEq(erc1155helper.balanceOf(alice, 1341), 500);
+        assertEq(token.balanceOf(alice, 1337), 100);
+        assertEq(token.balanceOf(alice, 1338), 200);
+        assertEq(token.balanceOf(alice, 1339), 300);
+        assertEq(token.balanceOf(alice, 1340), 400);
+        assertEq(token.balanceOf(alice, 1341), 500);
     }
 
     function testBatchBalanceOf() public {
@@ -117,14 +194,13 @@ contract ERC1155YulTest is Test {
         ids[3] = 1340;
         ids[4] = 1341;
 
-        erc1155helper.mint(address(0xBEEF), 1337, 100, "");
-        erc1155helper.mint(address(0xCAFE), 1338, 200, "");
-        erc1155helper.mint(address(0xFACE), 1339, 300, "");
-        erc1155helper.mint(address(0xDEAD), 1340, 400, "");
-        erc1155helper.mint(address(0xFEED), 1341, 500, "");
+        token.mint(address(0xBEEF), 1337, 100, "");
+        token.mint(address(0xCAFE), 1338, 200, "");
+        token.mint(address(0xFACE), 1339, 300, "");
+        token.mint(address(0xDEAD), 1340, 400, "");
+        token.mint(address(0xFEED), 1341, 500, "");
 
-        uint256[] memory balances = erc1155helper.balanceOfBatch(tos, ids);
-
+        uint256[] memory balances = token.balanceOfBatch(tos, ids);
         assertEq(balances[0], 100);
         assertEq(balances[1], 200);
         assertEq(balances[2], 300);
@@ -133,49 +209,43 @@ contract ERC1155YulTest is Test {
     }
 
     function testSafeTransferFromSelf() public {
-        erc1155helper.mint(address(this), 1337, 100, "");
+        token.mint(address(this), 1337, 100, "");
 
-        erc1155helper.safeTransferFrom(
-            address(this),
-            address(0xBEEF),
-            1337,
-            70,
-            ""
-        );
+        token.safeTransferFrom(address(this), address(0xBEEF), 1337, 70, "");
 
-        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1337), 70);
-        assertEq(erc1155helper.balanceOf(address(this), 1337), 30);
+        assertEq(token.balanceOf(address(0xBEEF), 1337), 70);
+        assertEq(token.balanceOf(address(this), 1337), 30);
     }
 
     function test_SafeTransferFromToEOA() public {
         address from = address(0xABCD);
 
-        erc1155helper.mint(from, 1337, 100, "");
+        token.mint(from, 1337, 100, "");
 
         vm.prank(from);
-        erc1155helper.setApprovalForAll(address(this), true);
+        token.setApprovalForAll(address(this), true);
 
         vm.expectEmit(false, true, true, true);
         emit TransferSingle(address(this), from, address(0xBEEF), 1337, 70);
 
-        erc1155helper.safeTransferFrom(from, address(0xBEEF), 1337, 70, "");
+        token.safeTransferFrom(from, address(0xBEEF), 1337, 70, "");
 
-        assertEq(erc1155helper.balanceOf(address(0xBEEF), 1337), 70);
-        assertEq(erc1155helper.balanceOf(from, 1337), 30);
+        assertEq(token.balanceOf(address(0xBEEF), 1337), 70);
+        assertEq(token.balanceOf(from, 1337), 30);
     }
 
     function test_SetApprovalForAll() public {
         vm.expectEmit(false, true, true, true);
         emit ApprovalForAll(address(this), alice, true);
-        erc1155helper.setApprovalForAll(alice, true);
-        bool isApproved = erc1155helper.isApprovedForAll(address(this), alice);
+        token.setApprovalForAll(alice, true);
+        bool isApproved = token.isApprovedForAll(address(this), alice);
         assertEq(isApproved, true);
-        erc1155helper.setApprovalForAll(alice, false);
-        isApproved = erc1155helper.isApprovedForAll(address(this), alice);
+        token.setApprovalForAll(alice, false);
+        isApproved = token.isApprovedForAll(address(this), alice);
         assertEq(isApproved, false);
         vm.prank(alice);
-        erc1155helper.setApprovalForAll(bob, true);
-        isApproved = erc1155helper.isApprovedForAll(alice, bob);
+        token.setApprovalForAll(bob, true);
+        isApproved = token.isApprovedForAll(alice, bob);
         assertEq(isApproved, true);
     }
 
@@ -194,8 +264,8 @@ contract ERC1155YulTest is Test {
         // we need to bound to below uint160.max since the storage would overflow
         // since the hash from storageSlot 0 is already a pretty big number so there
         // is only a certain amount of "ids" we can store from that point in storage
-        erc1155helper.mint(to, id, amount, mintData);
-        assertEq(erc1155helper.balanceOf(to, id), amount);
+        token.mint(to, id, amount, mintData);
+        assertEq(token.balanceOf(to, id), amount);
     }
 
     function test_Fuzz_SafeTransferFromToEOA(
@@ -216,27 +286,18 @@ contract ERC1155YulTest is Test {
 
         address from = address(0xABCD);
 
-        erc1155helper.mint(from, id, mintAmount, mintData);
+        token.mint(from, id, mintAmount, mintData);
 
         vm.prank(from);
-        erc1155helper.setApprovalForAll(address(this), true);
+        token.setApprovalForAll(address(this), true);
 
-        erc1155helper.safeTransferFrom(
-            from,
-            to,
-            id,
-            transferAmount,
-            transferData
-        );
+        token.safeTransferFrom(from, to, id, transferAmount, transferData);
 
         if (to == from) {
-            assertEq(erc1155helper.balanceOf(to, id), mintAmount);
+            assertEq(token.balanceOf(to, id), mintAmount);
         } else {
-            assertEq(erc1155helper.balanceOf(to, id), transferAmount);
-            assertEq(
-                erc1155helper.balanceOf(from, id),
-                mintAmount - transferAmount
-            );
+            assertEq(token.balanceOf(to, id), transferAmount);
+            assertEq(token.balanceOf(from, id), mintAmount - transferAmount);
         }
     }
 
@@ -251,9 +312,9 @@ contract ERC1155YulTest is Test {
         vm.assume(to != address(7));
         vm.assume(to != address(8));
         vm.assume(to != address(9));
-        erc1155helper.setApprovalForAll(to, approved);
+        token.setApprovalForAll(to, approved);
 
-        assertEq(erc1155helper.isApprovedForAll(address(this), to), approved);
+        assertEq(token.isApprovedForAll(address(this), to), approved);
     }
 
     // ------------------------------------------------- //
@@ -265,7 +326,7 @@ contract ERC1155YulTest is Test {
         uint256 amount,
         bytes memory data
     ) public {
-        erc1155helper.mint(address(0), id, amount, data);
+        token.mint(address(0), id, amount, data);
     }
 
     function testFail_Fuzz_BatchMintToZero() public {
@@ -283,7 +344,7 @@ contract ERC1155YulTest is Test {
         mintAmounts[3] = 400;
         mintAmounts[4] = 500;
 
-        erc1155helper.batchMint(address(0), ids, mintAmounts, "");
+        token.batchMint(address(0), ids, mintAmounts, "");
     }
 
     function testFail_Fuzz_BatchMintWithArrayMismatch() public {
@@ -300,7 +361,7 @@ contract ERC1155YulTest is Test {
         amounts[2] = 300;
         amounts[3] = 400;
 
-        erc1155helper.batchMint(address(0xBEEF), ids, amounts, "");
+        token.batchMint(address(0xBEEF), ids, amounts, "");
     }
 
     function testFail_Fuzz_SafeTransferFromInsufficientBalance(
@@ -319,18 +380,12 @@ contract ERC1155YulTest is Test {
             type(uint256).max
         );
 
-        erc1155helper.mint(from, id, mintAmount, mintData);
+        token.mint(from, id, mintAmount, mintData);
 
         vm.prank(from);
-        erc1155helper.setApprovalForAll(address(this), true);
+        token.setApprovalForAll(address(this), true);
 
-        erc1155helper.safeTransferFrom(
-            from,
-            to,
-            id,
-            transferAmount,
-            transferData
-        );
+        token.safeTransferFrom(from, to, id, transferAmount, transferData);
     }
 
     function testFail_Fuzz_SafeTransferFromSelfInsufficientBalance(
@@ -347,8 +402,8 @@ contract ERC1155YulTest is Test {
             type(uint256).max
         );
 
-        erc1155helper.mint(address(this), id, mintAmount, mintData);
-        erc1155helper.safeTransferFrom(
+        token.mint(address(this), id, mintAmount, mintData);
+        token.safeTransferFrom(
             address(this),
             to,
             id,
@@ -366,8 +421,8 @@ contract ERC1155YulTest is Test {
     ) public {
         transferAmount = bound(transferAmount, 1, mintAmount);
 
-        erc1155helper.mint(address(this), id, mintAmount, mintData);
-        erc1155helper.safeTransferFrom(
+        token.mint(address(this), id, mintAmount, mintData);
+        token.safeTransferFrom(
             address(this),
             address(0),
             id,
